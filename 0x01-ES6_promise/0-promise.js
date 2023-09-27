672a9c6dfb20ecb97ed9b135a6f46ad13f2d40d6
briankmbugua <briankmbuguak@gmail.com>
2023-09-27 15:16:31 +0300

    promise in javascript

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
-export function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        resolve(true);
-    });
+export default function getResponseFromAPI() {
+    return new Promise(() => { });
 }
