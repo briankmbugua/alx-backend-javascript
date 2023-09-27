8d4f94703fe30614f3fe3fdffcfe45fa4d587917
briankmbugua <briankmbuguak@gmail.com>
2023-09-27 18:22:17 +0300

    handling a promise object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,6 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resove, reject) => {
-        if (promise) {
-            resove({
-                status: 200,
-                body: 'Success'
-            })
-        }
-        else {
-            reject(new Error())
-        }
-    }).finally(() => {
-        console.log('Got a response from the API')
-    })
+    promise.then({ 'status': 200, 'body': 'Success' })
+        .catch(() => new Error())
+        .finally(() => console.log('Got a response from the API'));
 }
