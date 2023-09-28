366a33f9ff9f42da0af82d2e9028e2bf4a560ace
briankmbugua <briankmbuguak@gmail.com>
2023-09-28 03:52:25 +0300

    made e.message into a string

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,11 @@
 /* eslint-disable */
-function guardrail(mathFunction) {
+export default function guardrail(mathFunction) {
     let queue = [];
     try {
         queue.push(mathFunction());
     } catch (e) {
-        queue.push(e.message);
+        queue.push(String(e.message));
     }
     queue.push("Guardrail was processed");
     return queue;
 }
-
-export default guardrail;
