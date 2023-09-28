bd88774f9949e08918cd9558c9471935d55ef213
briankmbugua <briankmbuguak@gmail.com>
2023-09-28 04:07:40 +0300

    corrections

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,4 @@
 /* eslint-disable */
-export default function handleResponseFromAPI(promise) {
-    promise.then({ 'status': 200, 'body': 'Success' })
-        .catch(() => new Error())
-        .finally(() => console.log('Got a response from the API'));
+function handleResponseFromAPI(promise) {
+    return promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => console.log('Got a response from the API'));
 }
