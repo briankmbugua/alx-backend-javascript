34172ad167dfb1895d7df0b81354b1ac5a25b98e
briankmbugua <briankmbuguak@gmail.com>
2023-09-28 03:54:27 +0300

    made e into a string

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     try {
         queue.push(mathFunction());
     } catch (e) {
-        queue.push(String(e.message));
+        queue.push(String(e));
     }
     queue.push("Guardrail was processed");
     return queue;
