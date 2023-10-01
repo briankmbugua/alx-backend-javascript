39398a0cccaeedab85d06c3b93292d55b8a32642
briankmbugua <briankmbuguak@gmail.com>
2023-10-02 00:20:54 +0300

    for of

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
     for (const idx of array) {
-        const value = array[idx];
-        array[idx] = appendString + value;
+        array[idx] = appendString + array[idx];
     }
 
     return array;
