1ecdeb2cd5fab5a06a2a6ab3427428410dc54b76
briankmbugua <briankmbuguak@gmail.com>
2023-10-02 02:47:44 +0300

    report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,9 @@
 /* eslint-disable */
 export default function createReportObject(employeesList) {
-    const employees = employeesList;
-
     return {
-        average: employees.reduce((acc, employee) => acc + employee.income, 0) / employees.length,
-        employeeCount: employees.length,
-        max: Math.max(...employees.map(employee => employee.income)),
-        min: Math.min(...employees.map(employee => employee.income)),
+        allEmployees: employeesList,
+        getNumberOfDepartments() {
+            return Object.keys(this.allEmployees).length;
+        },
     };
 }
