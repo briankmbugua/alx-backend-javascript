1c776ba9f3962f2820b13fb653d53d1701ffedb6
briankmbugua <briankmbuguak@gmail.com>
2023-10-08 22:48:03 +0300

    update unique items in a map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function updateUniqueItems(map) {
+    map.forEach((value, key) => {
+        if (value === 1) {
+            map.set(key, 100);
+        }
+    });
+}
