229101b09f3fa12ec7611dd2b195a9a4a1acbb5d
briankmbugua <briankmbuguak@gmail.com>
2023-10-08 23:59:55 +0300

    weakMap data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,15 @@
+/* eslint-disable */
 const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
-    if (!weakMap.has(endpoint)){
-        weakMap.set(endpoint, 0)
-    }
-
-    const queryCount = weakMap.get(endpoint) + 1;
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  const queryCount = weakMap.get(endpoint) + 1;
     weakMap.set(endpoint, queryCount);
-
-    if (queryCount >= 5) {
-        throw new Error('Endpoint load is high')
+      if (queryCount >= 5) {
+        throw new Error('Endpoint load is high');
     }
-}
+  }
 
-export {queryAPI, weakMap};
+export { queryAPI, weakMap };
