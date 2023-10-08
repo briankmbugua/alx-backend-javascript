63232772a0bf19a2c553bc66b00d3a3bd5d42abd
briankmbugua <briankmbuguak@gmail.com>
2023-10-08 20:53:00 +0300

    set in javascript

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function hasValuesFromArray(set, array) {
+    for (let element of array) {
+        if (!set.has(element)) {
+            return false;
+        }
+    }
+    return true;
+}
